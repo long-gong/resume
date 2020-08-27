6becd0984b546ef5792c3626f729bd05906ad9e4
Long Gong <long.github@gmail.com>
2020-08-26 18:31:58 -0700

    remove texlive

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,8 +5,6 @@ set -e
 sudo add-apt-repository -y universe
 sudo apt-get -q update
 
-
-
 function install_miktex {
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
     if [[ $(lsb_release -rs) == "18.04" ]]; then # replace 8.04 by the number of release you want
@@ -29,4 +27,4 @@ then
     install_miktex
 fi
 
-sudo apt-get install -y texlive rubber
+sudo apt-get install -y rubber
